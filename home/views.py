2142e9ea66d07c07cb2f52e962025ba95233bc41
Satyam Singh <>
2024-08-29 10:08:29 +0530

    Templates Engin

--- home/views.py
+++ home/views.py
@@ -3,7 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,"index.html")
+    peoples = [
+        {"name": "Satyam Singh", "age":24},
+        {"name": "Adarsh", "age":44},
+        {"name": "Bhawesh", "age":84},
+        {"name": "Vinayak", "age":24}
+    ]
+    return render(request, "index.html", context={'peoples': peoples})
 
 def sucess(request):
     return HttpResponse("<H1>Hey! This is the Sucess Page</h1>")
